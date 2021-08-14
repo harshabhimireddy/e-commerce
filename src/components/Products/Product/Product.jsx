67a6ad8e4c1d0462a626ce624a9d1e1271bfdaec
Harsha Bhimireddy <harsha.bhimireddy@thebetmakers.com>
2021-08-15 00:40:26 +1000

    first commit

---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core'
+
+const Product = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Product
