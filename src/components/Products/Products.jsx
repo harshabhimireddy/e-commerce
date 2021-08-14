2a463d310bb1b02aa61ac10a7061259e965647fa
Harsha Bhimireddy <harsha.bhimireddy@thebetmakers.com>
2021-08-15 01:00:48 +1000

    Third commit

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,10 +1,10 @@
 import React from "recat";
 import Grid from "@material-ui/core";
-
+import Product from "./Product/Product";
 
 const products = [
-    { id: 1, name: 'Shoes', description: 'Running Shoes' },
-    { id: 2, name: 'Macbook', description: 'Apple Macbook' },
+    { id: 1, name: 'Shoes', description: 'Running Shoes', price: '$5' },
+    { id: 2, name: 'Macbook', description: 'Apple Macbook', price: '$10' },
 ];
 
 const Products = () => {
@@ -12,7 +12,7 @@ const Products = () => {
     <Grid conatiner justify="center" spacing={4}>
         {products.map((product) => (
             <Grid item key={product.id} xs={12} sm={6} lg={3}>
-                <Product/>
+                <Product product={product}/>
             </Grid>
         ))}
     </Grid>
